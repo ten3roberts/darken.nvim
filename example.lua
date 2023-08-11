6be4a1e8d6e1b95f9f7dfd8838ba303c8c5b5405
Tei Leelo Roberts <ten3roberts@gmail.com>
2023-08-11 15:01:07 +0200

    fix: statusline highlights

---
+++ example.lua
@@ -0,0 +1,4 @@
+function foo()
+end
+function bar()
+end
---
+++ init.lua
@@ -0,0 +1,52 @@
+-- DO NOT change the paths and don't remove the colorscheme
+local root = vim.fn.fnamemodify("./.repro", ":p")
+
+-- set stdpaths to use .repro
+for _, name in ipairs({ "config", "data", "state", "cache" }) do
+	vim.env[("XDG_%s_HOME"):format(name:upper())] = root .. "/" .. name
+end
+
+-- bootstrap lazy
+local lazypath = root .. "/plugins/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"--single-branch",
+		"https://github.com/folke/lazy.nvim.git",
+		lazypath,
+	})
+end
+vim.opt.runtimepath:prepend(lazypath)
+
+-- install plugins
+local plugins = {
+	"folke/tokyonight.nvim",
+	{
+		"stevearc/aerial.nvim",
+		config = function()
+			require("aerial").setup({
+				-- add your aerial config here
+			})
+		end,
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = { "c", "lua" },
+				auto_install = true,
+				highlight = { enable = true },
+			})
+		end,
+	},
+	-- add any other plugins here
+}
+require("lazy").setup(plugins, {
+	root = root .. "/plugins",
+})
+
+vim.cmd.colorscheme("tokyonight")
+-- add anything else here
